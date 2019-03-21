d679cde0d0d2c24752ae78c180dfcc2c70639e0e
Vladimir <kobzarenkovolodymyr@gmail.com>
2019-03-21 22:42:12 +0200

    Re-factored tests and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -65,15 +65,15 @@ class PrivatIngredientsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], ingredient.name)
 
     def test_create_ingredient_successful(self):
-    """Test creating a new ingredient"""
-    payload = {'name': 'Cabbage'}
-    self.client.post(INGREDIENTS_URL, payload)
+        """Test create a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
 
-    exists = Ingredient.objects.filter(
-        user=self.user,
-        name=payload['name']
-    ).exists()
-    self.assertTrue(exists)
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name'],
+        ).exists()
+        self.assertTrue(exists)
 
     def test_create_ingredient_invalid(self):
         """Test creating invalid ingredient fails"""
